c730fb81a29c230771d16d8cfe2b4d6b1229a481
Ryan Poolos <ryanpoolos@Gmail.com>
2012-08-25 10:41:35 -0400

    Laying a rad framework. Starting images.

--- Construct/Construct/GCViewController.m
+++ Construct/Construct/GCViewController.m
@@ -18,6 +18,20 @@
 {
     [super viewDidLoad];
 	// Do any additional setup after loading the view, typically from a nib.
+    
+    UIScrollView *map = [[UIScrollView alloc] initWithFrame:self.view.bounds];
+    [map setAutoresizingMask:self.view.autoresizingMask];
+    [map setBackgroundColor:[UIColor colorWithPatternImage:[UIImage imageNamed:@"backgroundTile.png"]]];
+    [map setContentSize:CGSizeMake(2048.0, 1536.0)];
+    
+    
+    UIButton *addButton = [UIButton buttonWithType:UIButtonTypeContactAdd];
+    [addButton setCenter:CGPointMake(32.0, 736.0)];
+    [addButton addTarget:self action:@selector(addBlock:) forControlEvents:UIControlEventTouchUpInside];
+    
+    
+    [self.view addSubview:map];
+    [self.view addSubview:addButton];
 }
 
 - (void)viewDidUnload
@@ -29,6 +43,11 @@
 - (BOOL)shouldAutorotateToInterfaceOrientation:(UIInterfaceOrientation)interfaceOrientation
 {
     return YES;
+}
+
+- (void)addBlock:(id)sender
+{
+    NSLog(@"Add a Block");
 }
 
 @end
